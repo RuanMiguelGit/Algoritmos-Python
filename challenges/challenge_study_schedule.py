615b5efd4aa6e5a72506c953b3349de602cf9ba4
Ruan Miguel <ruanmiguelps@gmail.com>
2021-09-15 14:27:44 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    and
+    contador = 0
+    try:
+        for time in permanence_period:
+            if target_time >= time[0] and target_time <= time[1]:
+                contador += 1
+    except TypeError:
+        return None
+    return contador
